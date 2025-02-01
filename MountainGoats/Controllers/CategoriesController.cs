2fab565eea9b9cae063286d55a7ef3d4e59e06f5
svenkat1-git <svenkat1@my.westga.edu>
2025-02-01 13:14:27 -0600

    created categoriescontroller to access prod.categories data

---
+++ MountainGoats/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MountainGoats.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MountainGoats.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly string _connectionString;
+
+        public CategoriesController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("BikeStoresConnection");
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = new List<Category>();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT category_id, category_name FROM production.categories";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            categories.Add(new Category
+                            {
+                                CategoryId = reader.GetInt32(0),
+                                CategoryName = reader.GetString(1)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return View(categories);
+        }
+    }
+}
