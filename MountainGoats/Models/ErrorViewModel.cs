4081d5c6c4643d199df2178ee9de32f3aa05088a
svenkat1-git <svenkat1@my.westga.edu>
2025-02-01 13:00:40 -0600

    Add project files.

---
+++ MountainGoats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MountainGoats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
