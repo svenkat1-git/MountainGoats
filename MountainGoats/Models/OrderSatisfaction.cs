95a8457536108092791ae472dd9e1151160f73c4
svenkat1-git <svenkat1@my.westga.edu>
2025-02-09 14:37:59 -0600

    defines data structure for ordersatisfaction table

---
+++ MountainGoats/Models/OrderSatisfaction.cs
@@ -0,0 +1,9 @@
+namespace MountainGoatsBikes.Models
+{
+    public class OrderSatisfaction
+    {
+        public int SatId { get; set; }
+        public int SatisfactionLevel { get; set; }
+        public int OrderId { get; set; }
+    }
+}
