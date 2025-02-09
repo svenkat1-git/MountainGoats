365df4431ac015957f51dbda49201c992697fb56
svenkat1-git <svenkat1@my.westga.edu>
2025-02-09 14:34:56 -0600

    defines data structure for stock table

---
+++ MountainGoats/Models/Stock.cs
@@ -0,0 +1,9 @@
+namespace MountainGoatsBikes.Models
+{
+    public class Stock
+    {
+        public int StoreId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
