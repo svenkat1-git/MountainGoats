e412cdf3847409106646655277aff2765a0ced11
svenkat1-git <svenkat1@my.westga.edu>
2025-02-09 14:34:27 -0600

    defines data structure for product table

---
+++ MountainGoats/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace MountainGoatsBikes.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public short ModelYear { get; set; }
+        public decimal ListPrice { get; set; }
+    }
+}
