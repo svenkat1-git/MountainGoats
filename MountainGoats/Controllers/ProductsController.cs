4c3aff8499c451dd7a2ff9043f0b241f1d373957
svenkat1-git <svenkat1@my.westga.edu>
2025-02-09 14:24:37 -0600

    Reads data from production.products

---
+++ MountainGoats/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using MountainGoatsBikes.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace MountainGoatsBikes.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly string _connectionString;
+		public ProductsController(IConfiguration configuration)
+		{
+			_connectionString = configuration.GetConnectionString("BikeStores");
+		}
+
+		public IActionResult Index()
+		{
+			List<Product> products = new List<Product>();
+			using (SqlConnection conn = new SqlConnection(_connectionString))
+			{
+				string query = "SELECT product_id, product_name, brand_id, category_id, model_year, list_price FROM production.products";
+				SqlCommand cmd = new SqlCommand(query, conn);
+				conn.Open();
+				using (SqlDataReader reader = cmd.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						products.Add(new Product
+						{
+							ProductId = reader.GetInt32(0),
+							ProductName = reader.GetString(1),
+							BrandId = reader.GetInt32(2),
+							CategoryId = reader.GetInt32(3),
+							ModelYear = reader.GetInt16(4),
+							ListPrice = reader.GetDecimal(5)
+						});
+					}
+				}
+			}
+			return View(products);
+		}
+	}
+}
