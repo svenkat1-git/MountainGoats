b53a4b6757d7a05a39598f6fe3a782534caa02ab
svenkat1-git <svenkat1@my.westga.edu>
2025-02-09 15:04:52 -0600

    removing redundant static files command

--- MountainGoats/Program.cs
+++ MountainGoats/Program.cs
@@ -22,8 +22,7 @@ app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
