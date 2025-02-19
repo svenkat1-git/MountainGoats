ec1c6394d68545c6f09190283eea5a345f971bf0
svenkat1-git <svenkat1@my.westga.edu>
2025-02-18 21:19:01 -0600

    adding paging info for customer page

--- MountainGoats/Models/Customer.cs
+++ MountainGoats/Models/Customer.cs
@@ -1,15 +1,24 @@
 namespace MountainGoatsBikes.Models
 {
-	public class Customer
-	{
-		public int CustomerId { get; set; }
+    public class Customer
+    {
+        public int CustomerId { get; set; }
         public string FirstName { get; set; } = string.Empty;
-		public string LastName { get; set; } = string.Empty;
-		public string? Phone { get; set; }
-		public string Email { get; set; } = string.Empty;
-		public string? Street { get; set; }
-		public string? City { get; set; }
-		public string? State { get; set; }
-		public string? ZipCode { get; set; }
-	}
+        public string LastName { get; set; } = string.Empty;
+        public string? Phone { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+    }
+
+    // define a second class for paging:
+    public class CustomerList
+    {
+        public List<Customer> Customers { get; set; } = new List<Customer>();
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRecords { get; set; }
+    }
 }
