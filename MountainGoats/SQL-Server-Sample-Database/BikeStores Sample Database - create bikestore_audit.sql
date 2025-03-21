a99f64e4b01b97c0f2d4b08581812a19b321d739
svenkat1-git <svenkat1@my.westga.edu>
2025-03-21 09:28:25 -0500

    • Create the following table based on the ERD below - bikestore_audit

---
+++ MountainGoats/SQL-Server-Sample-Database/BikeStores Sample Database - create bikestore_audit.sql
@@ -0,0 +1,11 @@
+USE BikeStores;
+GO
+
+CREATE TABLE sales.bikestore_audit
+(
+    id BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    audit_timestamp DATETIME NOT NULL CONSTRAINT DF_bikestore_audit_audit_timestamp DEFAULT GETDATE(),
+    activity_type VARCHAR(100) NULL,  -- This column can hold values such as "order created", "customer added", etc.
+    record_info NTEXT NOT NULL        -- Contains order details like order id, customer id, staff id, store id
+);
+GO
